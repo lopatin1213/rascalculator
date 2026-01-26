fdac42331f2716a4a2cc0d0610c518781a7290df
lopatin1213 <lopatinras@mail.ru>
2026-01-26 09:25:20 +0300

    Update views.py

--- rascalk/version_check/views.py
+++ rascalk/version_check/views.py
@@ -5,8 +5,7 @@ from django.http import HttpResponse
 
 def version_check(request):
     # Версия вашего калькулятора
-    VERSION = "9.21.46.22"
-    return HttpResponse(VERSION)
+    VERSION = "9.21.49.23"
 def version_check_for_PRO(request):
     # Версия вашего калькулятора
     VERSION = "7.17.21.12"
