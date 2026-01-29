6ee6e0675384ff2386297a8b22ba2e0eafc284db
lopatin1213 <anton.lopatin.al@mail.ru>
2026-01-29 19:10:54 +0300

    Редактирование

--- rascalk/error_db/models.py
+++ rascalk/error_db/models.py
@@ -5,3 +5,5 @@ class ErrorLog(models.Model):
     error_type = models.TextField()
     version = models.CharField(max_length=50)
     source = models.CharField(max_length=200)
+    def __str__(self):
+        return f'{self.error_type} из {self.version}'
