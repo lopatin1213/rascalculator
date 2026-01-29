c0698a9fddc72c28800319055f1952d933abbdd4
lopatin1213 <anton.lopatin.al@mail.ru>
2026-01-29 19:24:55 +0300

    Редактирование

--- rascalk/version_check/views.py
+++ rascalk/version_check/views.py
@@ -2,10 +2,11 @@
 
 # Create your views here.
 from django.http import HttpResponse
-
+from for_adm.models import AppRasCalck
 def version_check(request):
     # Версия вашего калькулятора
-    VERSION = "9.21.49.23"
+    version = AppRasCalck.objects.filter(is_latest=True).first()
+    VERSION = version.version
     return HttpResponse(VERSION)
 def version_check_for_PRO(request):
     # Версия вашего калькулятора
